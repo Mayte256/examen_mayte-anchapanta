2d1d0d033b477b70b0ccb02b5b0af38a3c4354e1
Javier-Villarroel93 <tu_correo_de_github@ejemplo.com>
2025-11-27 15:44:47 -0500

    Primer commit en rama Vinueza

--- stack/app.py
+++ stack/app.py
@@ -21,7 +21,7 @@ HTML_PAGE = """
 <body>
     <div class="card">
         <h1>Vinueza App</h1>
-        <p>Aplicación funcionando correctamente con diseño moderno.</p>
+        <p>Aplicación funcionando correctamente con diseño modernoooo.</p>
         <a href="/api" class="btn">Ver API JSON</a>
     </div>
 </body>
